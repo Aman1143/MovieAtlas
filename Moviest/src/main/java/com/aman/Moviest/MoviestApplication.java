3f23737090f2598ecb707eafdb4372e5a8e2be7b
Aman1143 <nandaaman1234@gmail.com>
2024-10-10 12:19:49 +0530

    fix the bug both frontend and backend

---
+++ Moviest/src/main/java/com/aman/Moviest/MoviestApplication.java
@@ -0,0 +1,32 @@
+package com.aman.Moviest;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class MoviestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviestApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
+
+}
